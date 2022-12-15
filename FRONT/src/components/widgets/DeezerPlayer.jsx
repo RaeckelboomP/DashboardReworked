fdec6a225fab1b200bf940dc702d6d951e5f1fda
Raeckelboom Pierre <pierre.raeckelboom@gmail.com>
2022-12-15 11:01:29 +0100

    CLEAN: Some code.

--- FRONT/src/components/widgets/DeezerPlayer.jsx
+++ FRONT/src/components/widgets/DeezerPlayer.jsx
@@ -3,12 +3,6 @@ import React from 'react';
 const DeezerPlayer = (props) => {
     let htmlData = props.widgetDatas.data;
     htmlData = htmlData.replace("height=\"300\"", "class=\"deezerPlayer\"");
-    /*
-    <iframe id="deezer-widget0" src="https://widget.deezer.com/widget/dark/album/9410100?app_id=457142&
-    autoplay=false&radius=true&tracklist=true"  height="300" allowtransparency="true" 
-    allowfullscreen="true" allow="encrypted-media">
-    </iframe>
-    */
 
     return (
         <div className="deezerPlayer" key={props.widgetDatas.idTab + "column1" + props.widgetDatas.id}
